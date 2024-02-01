cb65981a54ffe315b016022f3e22d43e1dc84df9
tomas barrera <tomesbarrera@gmail.com>
2024-02-01 18:17:26 -0300

    validaciones

--- app/Http/Requests/HorarioRequest.php
+++ app/Http/Requests/HorarioRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Carrera;
 use App\Models\Comision;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -23,17 +24,29 @@ class HorarioRequest extends FormRequest
      */
     public function rules(): array
     {
-        $id_primero = Comision::orderBy('id_comision', 'asc')->first()->id_comision;
-        $id_ultimo = Comision::orderBy('id_comision', 'desc')->first()->id_comision;
+        $id_primer_comision = Comision::orderBy('id_comision')->first()->id_comision;
+        $id_ultimo_comision = Comision::orderBy('id_comision', 'desc')->first()->id_comision;
+        
+        $id_primer_carrera=Carrera::orderBy('id_carrera')->first()->id_carrera;
+        $id_ultimo_carrera=Carrera::orderBy('id_carrera','desc')->first()->id_carrera;
+
         return [
             'comision' => [
                 'required',
                 'integer',
-                Rule::exists('comisiones', 'id_comision'), // Asegúrate de ajustar el nombre de la tabla si es diferente
-                'min:' . $id_primero,
-                'max:' . $id_ultimo,
+                Rule::exists('comisiones', 'id_comision'), // Utiliza la regla exists para validar que el valor proporcionado para 'comision' existe en la columna 'id_comision' de la tabla 'comisiones'
+                'min:' . $id_primer_comision,
+                'max:' . $id_ultimo_comision
                 
                 
+            ],
+            'carrera'=>[
+                'required',
+                'integer',
+                Rule::exists('carreras', 'id_carrera'), // Utiliza la regla exists para validar que el valor proporcionado para 'carrera' existe en la columna 'id_carrera' de la tabla 'carreras'
+                'min:'.$id_primer_carrera,
+                'max:'.$id_ultimo_carrera
+
             ],
         ];
     }
