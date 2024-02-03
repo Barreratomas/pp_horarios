9ea0a57fadf8d81b0f3d158aff24cd7529fac823
tomas barrera <tomesbarrera@gmail.com>
2024-02-03 15:49:34 -0300

    menu lateral

--- resources/views/layouts/parcials/menu.blade.php
+++ resources/views/layouts/parcials/menu.blade.php
@@ -1,7 +1,22 @@
-<nav>
-    <ul>
-        <li><a href="{{route('home')}}">home</a></li>
-        <li><a href="{{route('mostrarFormulario')}}">horarios</a></li>
-        
-    </ul>
-</nav>
+
+<div class="button-container">
+    <button id="toggleButton" class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i> 
+    </button>
+</div>
+
+<div class="collapse" id="sidebar">
+    <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar vh-100">
+        <div class="position-sticky">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('home')}}">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('mostrarFormulario')}}">horarios</a>
+                </li>
+                <!-- agregar mas -->
+            </ul>
+        </div>
+    </nav>
+</div>
